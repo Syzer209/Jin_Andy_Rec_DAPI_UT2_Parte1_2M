64e9057cd0fe7f1455ac7a2e6859bbcf481187f6
Syzer209 <113340696+Syzer209@users.noreply.github.com>
2023-04-16 12:14:54 +0200

    Parte 2

--- Parte1.py
+++ Parte1.py
@@ -1,39 +1,35 @@
-# Importamos la librería csv que permite trabajar con ficheros csv y convertirlos en un diccionario
 import csv
-import pprint as pp
 
-# Declaro el diccionario notas con las claves y listas vacías
-notas: dict = {
-    "Nombre": [],
-    "Apellido": [],
-    "Practica01": [],
-    "Practica02": [],
-    "Practica03": [],
-    "Examen": [],
-    "Recuperacion": [],
-    "Actitud": []
-}
+alumnado = []
+def process_class(ruta):
+   """
+   Lee los datos y los almacena en una lista de diccionarios donde en cada uno
+   de los alumnos las claves serán los datos de la cabecera del archivo CSV
+   y los valores serán los datos de cada alumno/a.
+   :param ruta: un str con la ruta del fichero (.csv) a abrir.
+   :return: None
+   """
+   global alumnado # He añadido esta linea de código porque sin el global no modificaba la lista alumnado quese encuentra fuera de la función
+   alumnado = []
+   with open(ruta, newline='', encoding="UTF-8") as csvfile:
+       reader = csv.DictReader(csvfile)
+       for fila in reader:
+           alumnado.append(fila)
+   return
 
-def process_class(path: str):
+def create_email(nombre, apellido):
     """
-    Pasa los datos del csv al diccionario
-
-    Args:
-        path (str): Ruta del fichero csv
+    Crea un correo electrónico con la primera letra del nombre y con las primeras 5 letras del apellido (Si las hubiera) y el dominio '@educacion.navarra.es'
+    :param nombre: Nombre del alumno
+    :param apellido: Apellido del alumno
+    :return: Un str con el email resultante
     """
-    with open(path, encoding="UTF-8") as file:
-        # Convierto el fichero csv en un diccionario en el que los valores de cada clave son las claves de la primera línea
-        file_dict = csv.DictReader(file)
-        # Recorro el diccionario y añade los valores a las listas del diccionario notas
-        for line in file_dict:
-            notas["Nombre"].append(line["Nombre"])
-            notas["Apellido"].append(line["Apellido"])
-            # Primero se convierten las comas en puntos para poder convertirlos a float y añadirlos a la lista
-            notas["Practica01"].append(float(line["Practica01"].replace(",", ".")))
-            notas["Practica02"].append(float(line["Practica02"].replace(",", ".")))
-            notas["Practica03"].append(float(line["Practica03"].replace(",", ".")))
-            notas["Examen"].append(float(line["Examen"].replace(",", ".")))
-            notas["Recuperacion"].append(float(line["Recuperacion"].replace(",", ".")))
-            notas["Actitud"].append(float(line["Actitud"].replace(",", ".")))
+    return nombre[0] + (apellido[0:5] if len(apellido) > 5 else apellido) + "@educacion.navarra.es"
 
 process_class("class.csv")
+
+correos = []
+for alumno in alumnado:
+    correos.append(create_email(alumno["Nombre"], alumno["Apellido"]))
+
+print(correos)
