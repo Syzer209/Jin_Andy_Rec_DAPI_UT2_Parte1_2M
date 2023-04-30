a6242f974279e66cea8d040ba4b6c95ed8b01056
Syzer209 <113340696+Syzer209@users.noreply.github.com>
2023-04-30 16:50:19 +0200

    Parte 3

--- Parte1.py
+++ Parte1.py
@@ -26,6 +26,21 @@ def create_email(nombre, apellido):
     """
     return nombre[0] + (apellido[0:5] if len(apellido) > 5 else apellido) + "@educacion.navarra.es"
 
+def calculate_grade(practica_01, practica_02, practica_03, examen, recuperacion, actitud):
+    '''
+    La función recibirá las notas de todos los apartados evaluados de cada alumno/a
+    y devolverá una nota final y un valor Verdadero/Falso en función de si ha aprobado o suspendido.
+    :param practica_01: Nota de la práctica 1
+    :param practica_02: Nota de la práctica 2
+    :param practica_03: Nota de la práctica 3
+    :param examen: Nota del examen
+    :param recuperacion: Nota de la recuperación
+    :param actitud: Nota de actitud
+    :return: Nota final y True si es mayor o igual a 5 o False en caso de ser menor que 5
+    '''
+    nota_final = (practica_01 + practica_02 + practica_03)/3 * 0.3 + max((examen, recuperacion)) * 0.6 + actitud * 0.1
+    return round(nota_final, 2), nota_final >= 5
+
 process_class("class.csv")
 
 correos = []
@@ -33,3 +48,5 @@ for alumno in alumnado:
     correos.append(create_email(alumno["Nombre"], alumno["Apellido"]))
 
 print(correos)
+
+
