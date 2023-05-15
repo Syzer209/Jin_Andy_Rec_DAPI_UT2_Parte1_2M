ffbcafdf6c1b5e1ce060adb3f122310c53899837
Syzer209 <113340696+Syzer209@users.noreply.github.com>
2023-05-15 22:51:23 +0200

    Parte 4

--- Parte1.py
+++ Parte1.py
@@ -1,21 +1,43 @@
 import csv
 
-alumnado = []
 def process_class(ruta):
-   """
-   Lee los datos y los almacena en una lista de diccionarios donde en cada uno
-   de los alumnos las claves serán los datos de la cabecera del archivo CSV
-   y los valores serán los datos de cada alumno/a.
-   :param ruta: un str con la ruta del fichero (.csv) a abrir.
-   :return: None
-   """
-   global alumnado # He añadido esta linea de código porque sin el global no modificaba la lista alumnado quese encuentra fuera de la función
-   alumnado = []
-   with open(ruta, newline='', encoding="UTF-8") as csvfile:
-       reader = csv.DictReader(csvfile)
-       for fila in reader:
-           alumnado.append(fila)
-   return
+    """
+    Lee los datos y los almacena en una lista de diccionarios donde en cada uno
+    de los alumnos las claves serán los datos de la cabecera del archivo CSV
+    y los valores serán los datos de cada alumno/a.
+    :param ruta: un str con la ruta del fichero (.csv) a abrir.
+    :return: None
+    """
+
+    # Leemos fichero CSV
+    alumnado = []
+    with open(ruta, newline='', encoding="UTF-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for fila in reader:
+            alumnado.append(fila)
+
+    grades = []
+    # Por cada alumno calculamos email y nota y metemos los datos en la lista
+    for alumno in alumnado:
+        email = create_email(alumno["Nombre"], alumno["Apellido"])
+        nota, aprobado = calculate_grade(float(alumno["Practica01"].replace(",", ".")), float(alumno["Practica02"].replace(",", ".")), float(alumno["Practica03"].replace(",", ".")),
+                                        float(alumno["Examen"].replace(",", ".")), float(alumno["Recuperacion"].replace(",", ".")), float(alumno["Actitud"].replace(",", ".")))
+        grades.append({
+           "Nombre": alumno["Nombre"],
+           "Apellido": alumno["Apellido"],
+           "Email": email,
+           "Nota": nota,
+           "Aprobado": aprobado
+        })
+
+    # Guardamos el diccionario en un fichero CSV
+    with open("grades.csv", "w", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=["Nombre", "Apellido", "Email", "Nota", "Aprobado"])
+        writer.writeheader()
+        for grade in grades:
+            writer.writerow(grade)
+
+    return
 
 def create_email(nombre, apellido):
     """
@@ -26,7 +48,7 @@ def create_email(nombre, apellido):
     """
     return nombre[0] + (apellido[0:5] if len(apellido) > 5 else apellido) + "@educacion.navarra.es"
 
-def calculate_grade(practica_01, practica_02, practica_03, examen, recuperacion, actitud):
+def calculate_grade(practica_01: float, practica_02: float, practica_03: float, examen: float, recuperacion: float, actitud: float):
     '''
     La función recibirá las notas de todos los apartados evaluados de cada alumno/a
     y devolverá una nota final y un valor Verdadero/Falso en función de si ha aprobado o suspendido.
@@ -43,10 +65,6 @@ def calculate_grade(practica_01, practica_02, practica_03, examen, recuperacion,
 
 process_class("class.csv")
 
-correos = []
-for alumno in alumnado:
-    correos.append(create_email(alumno["Nombre"], alumno["Apellido"]))
 
-print(correos)
 
 
